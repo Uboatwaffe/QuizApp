2a693c2ded6dd7bee56849c94d9b188a68827f87
Uboatwaffe <mpiatek1512@gmail.com>
2025-03-18 17:06:40 +0100

    Clean up project files; remove unused test method and suppress redundant warnings
    Signed-off-by: Uboatwaffe <mpiatek1512@gmail.com>

--- app/src/androidTest/java/com/example/quizapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/quizapp/ExampleInstrumentedTest.java
@@ -1,7 +1,6 @@
 package com.example.quizapp;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -11,7 +10,5 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-    }
+
 }
