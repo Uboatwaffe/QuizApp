eae93cec3aebfeeaa32e596dfdc41d09909a54e6
Uboatwaffe <mpiatek1512@gmail.com>
2025-03-23 22:45:27 +0100

    Add JUnit 5 dependency; update visibility of error message in sign-up fragment; rename navigation entry for closed questions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
---
+++ app/src/main/java/com/example/quizapp/db/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <p>Created on 23.03.2025</p>
+ * Package containing classes that are responsible for managing the database.
+ * @version 0.1
+ * @author Maciej
+ */
+package com.example.quizapp.db;
