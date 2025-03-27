cdf4684c1f1b141601245a79a544310cc64515b9
Uboatwaffe <mpiatek1512@gmail.com>
2025-03-27 10:01:46 +0100

    Add ChangeSetsFragment and related layout; update navigation for logged-in user interface

---
+++ app/src/main/java/com/example/quizapp/ui/logged_in/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <p>Created on 27.03.2025</p>
+ * Package for managing activities for logged-in users
+ *
+ * <ul>
+ *     <li> {@link com.example.quizapp.ui.logged_in.ChangeSetsFragment} - class for changing sets of questions </li>
+ *     <li> {@link com.example.quizapp.ui.logged_in.LoggedInFragment} - class for the menu for logged-in users </li>
+ * @version 0.1
+ * @author Maciej
+ */
+package com.example.quizapp.ui.logged_in;
---
+++ app/src/main/java/com/example/quizapp/ui/questions/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * <p>Created on 27.03.2025</p>
+ * Package for question related ui
+ *
+ * <ul>
+ *     <li> {@link com.example.quizapp.ui.questions.ClosedQuestionFragment} - class for the closed questions (single and multiple/true or false) </li>
+ *     <li> {@link com.example.quizapp.ui.questions.DateQuestionFragment} - class for the questions about date </li>
+ *     <li> {@link com.example.quizapp.ui.questions.OpenQuestionFragment} - class for the open questions </li>
+ *
+ * @version 0.1
+ * @author Maciej
+ */
+package com.example.quizapp.ui.questions;
