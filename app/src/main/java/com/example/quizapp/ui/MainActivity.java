3be15360b7b2b29ab2d644eeb7d0f7324ba0b56f
Uboatwaffe <mpiatek1512@gmail.com>
2025-04-25 16:43:14 +0200

    Comment out action bar setup to prevent exploitation; improve navigation control

--- app/src/main/java/com/example/quizapp/ui/MainActivity.java
+++ app/src/main/java/com/example/quizapp/ui/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         // Set up navigation controller
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration); // Commented out to avoid exploiting the action bar
     }
 
     /**
